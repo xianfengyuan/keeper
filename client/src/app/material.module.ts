53da31935edffadf312ec03a826beec98d0009d8
Xianfeng Yuan <xianfeng.yuan@menlosecurity.com>
2019-12-21 10:26:20 -0800

    adjust component sequence

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -5,21 +5,21 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatNativeDateModule } from '@angular/material';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material';
 import { MatOptionModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import {MatSortModule} from '@angular/material/sort';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -27,51 +27,52 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
    imports: [
       CommonModule,
+      MatBadgeModule,
       MatButtonModule,
       MatCardModule,
-      MatDividerModule,
-      MatToolbarModule,
-      MatIconModule,
-      MatSidenavModule,
-      MatBadgeModule,
-      MatListModule,
-      MatGridListModule,
-      MatFormFieldModule,
-      MatInputModule,
-      MatOptionModule,
-      MatSelectModule,
-      MatSnackBarModule,
-      MatRadioModule,
-      MatDatepickerModule,
-      MatNativeDateModule,
       MatChipsModule,
-      MatTooltipModule,
-      MatTableModule,
-      MatPaginatorModule
-   ],
-   exports: [
-      MatButtonModule,
-      MatCardModule,
+      MatDatepickerModule,
       MatDividerModule,
-      MatToolbarModule,
-      MatIconModule,
-      MatSidenavModule,
-      MatBadgeModule,
-      MatListModule,
-      MatGridListModule,
       MatFormFieldModule,
+      MatGridListModule,
+      MatIconModule,
       MatInputModule,
+      MatListModule,
+      MatNativeDateModule,
       MatOptionModule,
+      MatPaginatorModule,
+      MatRadioModule,
       MatSelectModule,
+      MatSidenavModule,
       MatSnackBarModule,
       MatSortModule,
-      MatRadioModule,
-      MatDatepickerModule,
-      MatNativeDateModule,
-      MatChipsModule,
-      MatTooltipModule,
       MatTableModule,
-      MatPaginatorModule
+      MatToolbarModule,
+      MatTooltipModule,
+   ],
+   exports: [
+      MatBadgeModule,
+      MatButtonModule,
+      MatCardModule,
+      MatChipsModule,
+      MatDatepickerModule,
+      MatDividerModule,
+      MatFormFieldModule,
+      MatGridListModule,
+      MatIconModule,
+      MatInputModule,
+      MatListModule,
+      MatNativeDateModule,
+      MatOptionModule,
+      MatPaginatorModule,
+      MatRadioModule,
+      MatSelectModule,
+      MatSidenavModule,
+      MatSnackBarModule,
+      MatSortModule,
+      MatTableModule,
+      MatToolbarModule,
+      MatTooltipModule,
    ],
    providers: [
       MatDatepickerModule,
