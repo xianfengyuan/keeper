6f107c0ad4ffea28a3c8406ee29ac0270eb6fc9b
Xianfeng Yuan <xianfeng.yuan@menlosecurity.com>
2020-02-24 11:56:47 -0800

    wait for db connection

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -1,9 +1,29 @@
 const config = require('../config.json');
 const mongoose = require('mongoose');
-mongoose.connect(
-    process.env.MONGODB_URI || config.connectionString, 
-    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
-);
+const mongoURL = process.env.MONGODB_URI || config.connectionString;
+
+async function initMongo() {
+    console.log('Initialising MongoDB...')
+    let success = false
+    while (!success) {
+        try {
+            client = await mongoose.connect(mongoURL,
+                { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+            success = true
+        } catch {
+            console.log('Error connecting to MongoDB, retrying in 1 second')
+            await new Promise(resolve => setTimeout(resolve, 1000))
+        }
+    }
+    console.log('MongoDB initialised')
+    return client
+}
+
+initMongo()
+// mongoose.connect(
+//     process.env.MONGODB_URI || config.connectionString,
+//     { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
+// );
 mongoose.Promise = global.Promise;
 
 module.exports = {
